d24dfa58c53635205416891cf0ec9a9cfd62509b
mattkastner <matthewkastner@gmail.com>
2020-05-02 15:36:33 -0600

    routes and header file
    made changes to the routes for the 'share' types added minor changes to the

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Link, withRouter } from 'react-router-dom'
 
 function Header(props) {
     return (
         <div>
-            Header.js
+            {
+                props.location.pathname.includes('share') ?
+                <ul>
+                    <Link to={{ pathname= '/share' }}><li>Culture</li></Link>
+                    <Link to={{ pathname= '/products/share' }}><li>Products</li></Link>
+                </ul>
+                :
+                <ul>
+                    <Link to={{ pathname= '/culture' }}><li>Culture</li></Link>
+                    <Link to={{ pathname= '/products' }}><li>Products</li></Link>
+                    <Link to={{ pathname= '/business' }}><li>Business</li></Link>
+                    <Link to={{ pathname= '/training' }}><li>Training</li></Link>
+                </ul>
+            }
         </div>
     );
 }
 
-export default Header;
+export default withRouter(Header);
