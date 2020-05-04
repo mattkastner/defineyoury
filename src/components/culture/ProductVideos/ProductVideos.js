160a4911bd2aa901e822d0815f239cf012fb5a9e
mattkastner <matthewkastner@gmail.com>
2020-05-03 19:28:52 -0600

    changes to products videos
    added code for the product videos

---
+++ src/components/culture/ProductVideos/ProductVideos.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+// style
+import "./ProductVideos.scss";
+
+function ProductVideos(props) {
+  return (
+    <section className="product-videos__container">
+      <div>
+        <h3>We are all in this together</h3>
+        <p>
+          After cleansing and balancing, your body now has the foundation it
+          needs to build toward prime health. You’ve set the stage to take a
+          targeted approach to your body Research and innovation are here to
+          empower you with information, products, and services. Take
+          responsibility for your health and quality of life! With over 400
+          products in over 50 countries!
+        </p>
+      </div>
+      <div>ProductVideos</div>
+    </section>
+  );
+}
+
+export default ProductVideos;
--- src/views/Culture.js
+++ src/views/Culture.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import FirstLook from '../components/culture/FirstLook/FirstLook'
-import ProductVideos from '../components/culture/ProductVideos'
+import ProductVideos from '../components/culture/ProductVideos/ProductVideos'
 import CultureText from '../components/culture/CultureText'
 import ActionCalls from '../components/culture/ActionCalls';
 
 function Culture(props) {
     return (
-        <div>
+        <div className="culture-view">
             <FirstLook />
             <ProductVideos />
             <CultureText />
