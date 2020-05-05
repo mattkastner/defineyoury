64f55378e9a9a08eec8d2b9c7dedaaf756147793
mattkastner <matthewkastner@gmail.com>
2020-05-04 23:26:21 -0600

    footer changes
    made small changes to the footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,22 +1,25 @@
 import React from "react";
 
-import './Footer.scss'
+import "./Footer.scss";
 
 function Footer(props) {
-    return (
-        <div className="footer-body">
-            <div className="about-us-container">
-                <div className="footer-topics">About Us</div>
-                <div className="about-us-text">Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed ….</div>
-            </div>
-            <div className="quick-links-container">
-                <div className="footer-topics">Quick Links</div>
-            </div>
-            <div className="social-media-container">
-                <div className="footer-topics">Social Media</div>
-            </div>
+  return (
+    <div className="footer-body">
+      <div className="about-us-container">
+        <div className="footer-topics">About Us</div>
+        <div className="about-us-text">
+          Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in
+          laying out print, graphic or web designs. The passage is attributed ….
         </div>
-    );
+      </div>
+      <div className="quick-links-container">
+        <div className="footer-topics">Quick Links</div>
+      </div>
+      <div className="social-media-container">
+        <div className="footer-topics">Social Media</div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
