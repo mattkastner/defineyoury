7e02cd5ee9a83885cf0a6ab49931c40f49fd8e5c
sjordan20 <jordanstephen949@gmail.com>
2020-05-08 16:46:14 -0600

    business video created

---
+++ src/components/business/businessVideos/BusinessVideosDisplay.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function BusinessVideosDisplay(props) {
+    return (
+        <div>
+            <div>
+                {props.text}
+            </div>
+        </div>
+    );
+}
+
+export default BusinessVideosDisplay;
