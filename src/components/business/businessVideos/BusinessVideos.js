d7da1ac0b1b96f3ecbfbca1b1b1dadaed6bb7aaa
sjordan20 <jordanstephen949@gmail.com>
2020-05-08 17:37:55 -0600

    video display

--- src/components/business/businessVideos/BusinessVideos.js
+++ src/components/business/businessVideos/BusinessVideos.js
@@ -6,17 +6,59 @@ function BusinessVideos(props) {
     return (
         <section>
             <div className='business-videos-body'>
-                <div className='video-container'>
-                    <h1 className='our-stories'>
-                        Our Stories
+                <h1 className='our-stories'>
+                    Our Stories
                     </h1>
+                <div className='business-video-container'>
+
+                    <div className='business-videos'>
+                        <BusinessVideosDisplay
+                            text='Shavonda Crawford'
+                            src='https://player.vimeo.com/video/115187166'
+                        />
+                        <BusinessVideosDisplay
+                            text='Jan Bloom'
+                        />
+                        <BusinessVideosDisplay
+                            text='Steve Farias'
+                        />
+                        <BusinessVideosDisplay
+                            text='Rick Gash'
+                        />
+                        <BusinessVideosDisplay
+                            text='April Judd'
+                        />
+                        <BusinessVideosDisplay
+                            text='Paul Magisrti'
+                        />
+                        <BusinessVideosDisplay
+                            text='Greg McTee'
+                        />
+                        <BusinessVideosDisplay
+                            text='Jim Moyles'
+                        />
+                        <BusinessVideosDisplay
+                            text='Todd Pettus'
+                        />
+                        <BusinessVideosDisplay
+                            text='Phillip Song'
+                        />
+                        <BusinessVideosDisplay
+                            text='Ron Thomson'
+                        />
+                        <BusinessVideosDisplay
+                            text='Laurie Turner'
+                        />
+                        <BusinessVideosDisplay
+                            text='Gundrun Van Dyke'
+                        />
+                        <BusinessVideosDisplay
+                            text='Evan Weston'
+                        />
+
+
+                    </div>
                 </div>
-                <BusinessVideosDisplay
-                    text='Jan Bloom'
-                />
-                <BusinessVideosDisplay
-                    text='Rav Malik'
-                />
             </div>
         </section>
     );
--- src/components/business/businessVideos/BusinessVideosDisplay.js
+++ src/components/business/businessVideos/BusinessVideosDisplay.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import './businessVideoDisplay.scss'
+
 function BusinessVideosDisplay(props) {
     return (
         <div>
-            <div>
-                {props.text}
+            <div className='story-video'>
+                <h1>
+                    {props.text}
+                </h1>
+                <video src={props.src} width='320' height='240' controls
+                >
+                </video>
+
             </div>
         </div>
     );
