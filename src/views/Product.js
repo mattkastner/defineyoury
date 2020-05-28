bd1f56d94b48b81208dfa025b369cfb6262ae484
sjordan20 <jordanstephen949@gmail.com>
2020-05-27 18:21:35 -0600

    product videos added

--- src/views/Product.js
+++ src/views/Product.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ProductDisplay from "../components/product/ProductDisplay";
+import CoreProducts from "../components/product/coreProducts/CoreProducts";
+
 
 function Product(props) {
   return (
     <div>
+      <CoreProducts />
       <ProductDisplay />
     </div>
   );
