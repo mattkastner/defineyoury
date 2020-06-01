2b477b9ac8f6b7f0a4b48f13306680302d3dbded
sjordan20 <jordanstephen949@gmail.com>
2020-06-01 15:53:02 -0600

    intro video player

--- src/components/business/introVideos/IntroVideosDisplay.js
+++ src/components/business/introVideos/IntroVideosDisplay.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import ReactPlayer from "react-player"
+import './introVideosDisplay.scss'
 
 function IntroVideosDisplay(props) {
     return (
-        <div>
-            {props.text}
+        <div className='intro-videos-display-body'>
+
+            <ReactPlayer className='intro-video-player' url={props.url} controls />
+            <h1>
+                {props.title}
+            </h1>
+            <p>
+                {props.text}
+            </p>
         </div>
     );
 }
