5d0709b5afebc6e096f9e84cefd9bef1ce0ea4a9
sjordan20 <jordanstephen949@gmail.com>
2020-06-02 13:15:36 -0600

    product-guide-menu

--- src/views/Product.js
+++ src/views/Product.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ProductDisplay from "../components/product/ProductDisplay";
 import CoreProducts from "../components/product/coreProducts/CoreProducts";
+import ProductGuide from '../components/product/productGuide/ProductGuide'
 
 
 function Product(props) {
   return (
     <div>
       <CoreProducts />
+      <ProductGuide />
       <ProductDisplay />
     </div>
   );
